2bbb5863811bb57db21f3d5af7b83cfffbca948a
asier <suarexasier@gmail.com>
2023-10-27 14:28:40 +0200

    Creado MainActiviy

--- Proyecto/app/src/main/java/com/example/proyectolenguajesignos/MainActivity.java
+++ Proyecto/app/src/main/java/com/example/proyectolenguajesignos/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.proyectolenguajesignos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecyclerDataAdapater recyclerDataAdapater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerDataAdapater = new RecyclerDataAdapter(list, new RecyclerDataAdapter.onItemClickListener(){
+            public void onItemClick(int posistion)
+        });
+
     }
 }
