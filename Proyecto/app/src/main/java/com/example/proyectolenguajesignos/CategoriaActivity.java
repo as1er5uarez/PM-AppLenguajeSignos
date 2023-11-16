04c3ca25f159748a61215c178ed85a1fdd3fccc9
asier <suarexasier@gmail.com>
2023-11-16 14:24:56 +0100

    feat: Abre el SignoActivity y muestra la imagen del signo junto a la palabra

--- Proyecto/app/src/main/java/com/example/proyectolenguajesignos/CategoriaActivity.java
+++ Proyecto/app/src/main/java/com/example/proyectolenguajesignos/CategoriaActivity.java
@@ -32,7 +32,7 @@ public class CategoriaActivity extends AppCompatActivity {
         signos.add(new Signo(R.drawable.comida2,"comida"));
         signos.add(new Signo(R.drawable.comida2,"comida"));
         ArrayList<Signo>signos1 =new ArrayList<>();
-        signos1.add(new Signo(R.drawable.comida2,"comida"));
+        signos1.add(new Signo(R.drawable.ropa1,"ropa"));
         categorias.add(new Categoria(1,R.drawable.comida2 ,"Categoría 1",signos));
         categorias.add(new Categoria(2,R.drawable.ropa1 ,"Categoría 2", signos1));
         categorias.add(new Categoria(3,R.drawable.comida2 ,"Categoría 1",signos));
